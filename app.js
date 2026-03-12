16f38139ab3efa1213d8d7c17cb7f71c4b1ec6b2
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-12 21:47:50 +0530

    register route tested

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 require('dotenv').config();
 // Initialize the Express application
 const express = require('express');
-const connectDB = require('./src/config/db');
 const app = express();
+
+// local file require
+const connectDB = require('./src/config/db');
+const authRoutes = require("./src/routes/authRoutes");
+
+// database connection
+connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB();
 
-// Routes
+// Routes test
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// main routes
+app.use("/api/auth",authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
