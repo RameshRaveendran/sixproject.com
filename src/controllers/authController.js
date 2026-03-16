e91d78c8da7a7028c08295f3174911b9376e0d28
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-03-16 12:37:56 +0530

    admin protection and user route tested

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -101,6 +101,7 @@ const loginUser = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Login successful",
+      token,
       data: {
         id: user._id,
         name: user.name,
